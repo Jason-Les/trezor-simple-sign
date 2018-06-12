d649b403cd00ae489e97ed66a35454a42c638c26
Jason-Les <jasonles@gmail.com>
2018-06-11 19:20:52 -0700

    Updated head comments

--- trezor_sign.py
+++ trezor_sign.py
@@ -1,12 +1,13 @@
 '''
-Very simple commandline tool for signing messages and Bitcoin transactions with Trezor
+Very simple tool for signing messages and transactions with trezor.
+This was written using the SatoshiLab's python-trezor API available here: https://github.com/trezor/python-trezor
 
 Please do not use for mainnet transactions without first reviewing and testing code yourself. Tool's intention is to be
-a proof of concept, not for regular use. I have not added sufficient error-checking on input.
+a proof of concept, **NOT for regular use**. I have not added sufficient error-checking on input. I have done minimal testing.
 
 Currently only supporting:
 
--P2PKH addresses
+-P2PKH testnet addresses
 -Single input (Single prev hash and index)
 -Single output (with a single change address)
 
